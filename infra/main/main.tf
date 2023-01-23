901190b0e61c8ff56fe4144d8f209e177af9eb2e
Luis Costa <luis.mfc@protonmail.com>
2023-01-23 16:51:04 +0000

    Add tags to private dns zone

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -31,6 +31,8 @@ resource "azurerm_subnet" "aci_sub" {
 resource "azurerm_private_dns_zone" "pdz" {
   name                = replace("${local.prefix}-pdz.azure.com", "-", ".")
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = var.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "dnszonelink" {
